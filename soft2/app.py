c38bea5e9d9e4e7e78f9f18201b14b91e6dac069
yezuna <73248669+yezuna@users.noreply.github.com>
2022-02-17 21:18:52 -0800

    Update app.py

--- soft2/app.py
+++ soft2/app.py
@@ -7,7 +7,6 @@ app = Flask(
   static_folder='static'
 )
 
-
 # password page
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -25,9 +24,24 @@ def login():
       s.close()
   return render_template('login.html')
 
-  if __name__ == '__main__':
-    app.run(
-    host='0.0.0.0',
-    debug=True,
-    port=8080
-    )
+# signup 
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+  error = None
+  if request.method == 'POST':
+      s = shelve.open('logins')
+      try:
+        s[request.form['username']]
+        error = "Invalid username or password. Please try again!"
+      except KeyError:
+        s[request.form['username']] = request.form['password']
+        return redirect(url_for('login'))
+      s.close()
+  return render_template('signup.html', error=error)
+
+if __name__ == '__main__':
+  app.run(
+  host='0.0.0.0',
+  debug=True,
+  port=8080
+  )
